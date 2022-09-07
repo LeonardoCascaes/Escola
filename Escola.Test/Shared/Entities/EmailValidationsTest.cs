5abd47f1ac79448993be510614d161f591f3572e
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-09-07 14:44:24 -0300

    Criado testes para as validações

---
+++ Escola.Test/Shared/Entities/EmailValidationsTest.cs
@@ -0,0 +1,38 @@
+﻿using Escola.Shared.Entities;
+using Xunit;
+
+namespace Escola.Test.Shared.Entities
+{
+    public class EmailValidationsTest
+    {
+        [Fact]
+        public void Nao_Deve_Retornar_Notificacao_Caso_O_Valor_Teste_Possua_Arroba()
+        {
+            //Arrange
+            var testValue = "test@test.com";
+            var validation = new Validation().Requires().HasAt("testValue", testValue);
+
+            //Act
+            var notifications = validation.Notifications;
+
+            //Assert
+            Assert.Equal(0, notifications.Count);
+            Assert.True(validation.IsValid());
+        }
+
+        [Fact]
+        public void Deve_Retornar_Notificacao_Caso_O_Valor_Teste_Nao_Possua_Arroba()
+        {
+            //Arrange
+            var testValue = "test.com";
+            var validation = new Validation().Requires().HasAt("testValue", testValue);
+
+            //Act
+            var notifications = validation.Notifications;
+
+            //Assert
+            Assert.Equal(1, notifications.Count);
+            Assert.False(validation.IsValid());
+        }
+    }
+}
